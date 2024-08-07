b94338512dce1e5b58e6093e21332be2c715a173
harshitkakadiya <harshitkakadiya01@gmail.com>
2024-08-07 09:56:34 +0530

    07-08

--- server.js
+++ server.js
@@ -6,9 +6,20 @@ const express = require('express')
 
 const server = express();
 
+const fs = require('fs');
+const data = fs.readFileSync('./friend.json','utf-8')
+
+let middleWare = (req, res, next)=>{
+    if(req.query.password === '1234'){
+        console.log('Success');
+        next();
+    }else{
+        return res.json({message: 'Incccorexxt Way!!!!!'})
+    }
+}
 // => CRUD -> Create(POST), Read(GET), Update(PUT, PATCH), Delete(DELETE)
 
-server.get("/",(req, res)=>{
+server.get("/", middleWare,(req, res)=>{
     res.write('Welcome To Express Server');
     res.end();
 })
@@ -27,29 +38,33 @@ server.post("/user",(req, res)=>{
     res.json({message:'User POST Method'});
 })
 
-server.put("/user",(req, res)=>{
-    // res.status(201);
-    res.json({message:'User PUT Method'});
-})
+// server.put("/user",(req, res)=>{
+//     // res.status(201);
+//     res.json({message:'User PUT Method'});
+// })
 
-server.patch("/user",(req, res)=>{
-    // res.status(201);
-    res.json({message:'User PATCH Method'});
-})
+// server.patch("/user",(req, res)=>{
+//     // res.status(201);
+//     res.json({message:'User PATCH Method'});
+// })
 
-server.delete("/user",(req, res)=>{
-    // res.status(201);
-    res.json({message:'User DELETE Method'});
-})
+// server.delete("/user",(req, res)=>{
+//     // res.status(201);
+//     res.json({message:'User DELETE Method'});
+// })
 
-server.patch("/admin",(req, res)=>{
-    // res.status(201);
-    res.json({message:'Admin PATCH Method'});
-})
+// server.patch("/admin",(req, res)=>{
+//     // res.status(201);
+//     res.json({message:'Admin PATCH Method'});
+// })
 
-server.delete("/admin",(req, res)=>{
-    // res.status(201);
-    res.json({message:'Admin DELETE Method'});
+// server.delete("/admin",(req, res)=>{
+//     // res.status(201);
+//     res.json({message:'Admin DELETE Method'});
+// })
+server.get("/friend", (req,res)=>{
+    res.status(200);
+    res.json(JSON.parse(data));
 })
 
 server.listen(1000,()=>{
